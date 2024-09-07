41fca09bf08bf6d6540b3d14aa4cdd4b915994f9
wasabina67 <wasabina67@gmail.com>
2024-09-07 12:51:11 +0900

    Update auto_commit_message.py

--- auto_commit_message.py
+++ auto_commit_message.py
@@ -7,35 +7,38 @@ def get_changes():
 
 
 def main():
-    changes = get_changes()
-    print(changes)
+    try:
+        changes = get_changes()
+        print(changes)
 
-    update_files = []
-    create_files = []
-    delete_files = []
-    rename_files = []
+        update_files = []
+        create_files = []
+        delete_files = []
+        rename_files = []
 
-    for change in changes:
-        if change == "":
-            continue
+        for change in changes:
+            if change == "":
+                continue
 
-        staging, working, space, file = \
-            change[0], change[1], change[2], change[3:]  # noqa
+            staging, working, space, file = \
+                change[0], change[1], change[2], change[3:]  # noqa
 
-        if staging == " " and space == " ":
-            continue
-        elif staging == "M" and space == " ":
-            update_files.append(file)
-        elif staging == "A" and space == " ":
-            create_files.append(file)
-        elif staging == "D" and space == " ":
-            delete_files.append(file)
-        elif staging == "R" and space == " ":
-            rename_files.append(file)
-        else:
-            raise Exception("aaa")
+            if staging == " " and space == " ":
+                continue
+            elif staging == "M" and space == " ":
+                update_files.append(file)
+            elif staging == "A" and space == " ":
+                create_files.append(file)
+            elif staging == "D" and space == " ":
+                delete_files.append(file)
+            elif staging == "R" and space == " ":
+                rename_files.append(file)
+            else:
+                raise Exception("aaa")
 
-    print(update_files, create_files, delete_files, rename_files)
+        print(update_files, create_files, delete_files, rename_files)
+    except Exception as ex:
+        print(ex)
 
 
 if __name__ == "__main__":
