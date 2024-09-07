c187c91d59efa810ad7fdbae891f476cb1f59510
wasabina67 <wasabina67@gmail.com>
2024-09-07 22:33:29 +0900

    Update auto_commit_message.py

--- auto_commit_message.py
+++ auto_commit_message.py
@@ -4,16 +4,27 @@ import subprocess
 def get_commit_message(files_tuple):
     message = ""
     update, create, delete, rename = files_tuple
-    for files, action in [(update, "Update"), (create, "Create"), (delete, "Delete"), (rename, "Rename")]:
+    for files, action in [
+        (update, "Update"),
+        (create, "Create"),
+        (delete, "Delete"),
+        (rename, "Rename"),
+    ]:
         if files:
             files_joined = ", ".join(files)
-            message = message + f" & {action} {files_joined}" if message else f"{action} {files_joined}"
+            message = (
+                message + f" & {action} {files_joined}"
+                if message
+                else f"{action} {files_joined}"
+            )
 
     return message
 
 
 def get_changes():
-    result = subprocess.run(["git", "status", "--short"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    result = subprocess.run(
+        ["git", "status", "--short"], stdout=subprocess.PIPE, stderr=subprocess.PIPE
+    )
     if result.returncode != 0:
         raise Exception(result.stderr.decode("utf-8"))
     return result.stdout.decode("utf-8").split("\n")
@@ -34,8 +45,7 @@ def main():
             if change == "":
                 continue
 
-            staging, working, space, file = \
-                change[0], change[1], change[2], change[3:]
+            staging, working, space, file = change[0], change[1], change[2], change[3:]
 
             if staging == "M" and space == " ":
                 update_files.append(file)
@@ -52,7 +62,9 @@ def main():
             else:
                 raise Exception(change)
 
-        message = get_commit_message(files_tuple=(update_files, create_files, delete_files, rename_files))
+        message = get_commit_message(
+            files_tuple=(update_files, create_files, delete_files, rename_files)
+        )
         if message:
             print(message)
     except Exception as ex:
