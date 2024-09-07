04d6d4c466d7594a68da8b5104bdebd56c6ccc76
wasabina67 <wasabina67@gmail.com>
2024-09-07 12:36:33 +0900

    Update auto_commit_message.py

--- auto_commit_message.py
+++ auto_commit_message.py
@@ -10,6 +10,18 @@ def main():
     changes = get_changes()
     print(changes)
 
+    update_files = []
+    create_files = []
+    delete_files = []
+    rename_files = []
+
+    for change in changes:
+        if change == "":
+            continue
+
+        staging, working, space, file = \
+            change[0], change[1], change[2], change[3:]  # noqa
+
 
 if __name__ == "__main__":
     main()
