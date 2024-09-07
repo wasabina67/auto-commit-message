3a667f85a821cb6bec7c795214dd9a3aafd8b990
wasabina67 <wasabina67@gmail.com>
2024-09-07 12:36:14 +0900

    Update get_changes()

--- auto_commit_message.py
+++ auto_commit_message.py
@@ -2,12 +2,12 @@ import subprocess
 
 
 def get_changes():
-    return
+    result = subprocess.run(["git", "status", "--short"], stdout=subprocess.PIPE)
+    return result.stdout.decode("utf-8").split("\n")
 
 
 def main():
-    result = subprocess.run(["git", "status", "--short"], stdout=subprocess.PIPE)
-    changes = result.stdout.decode("utf-8").split("\n")
+    changes = get_changes()
     print(changes)
 
 
